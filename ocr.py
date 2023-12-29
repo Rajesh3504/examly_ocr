95c4aa5c3dde8ecec41146e7d359e65145efe9e3
RAJESH PONNAGANTI <139054007+Rajesh3504@users.noreply.github.com>
2023-12-30 02:56:37 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,4 +1,4 @@
-import easyocr
+import easyocr as ocr  
 import streamlit as st  
 from PIL import Image  
 import numpy as np  
@@ -9,8 +9,6 @@ import google.generativeai as genai
 genai.configure(api_key='AIzaSyCbrGlCX60mFMpwmmB_jYJGZs7M21PpTj4')
 
 st.title("Chaduvko bro")
-
-
 
 st.markdown("")
 
@@ -33,38 +31,10 @@ elif input_option == "File":
         input_image = Image.open(image)
 
 # Load OCR model
-@st.cache
-def load_model():
-    reader = ocr.Reader(['en'], model_storage_directory='.')
-    return reader
-
-reader = load_model()  # Load model
-
-if 'input_image' in locals():
-    st.image(input_image)  # Display image
-
-    with st.spinner("Please wait, processing..."):
-
-        result = reader.readtext(np.array(input_image))
-
-        result_text = []  # Empty list for results
-
-        for text in result:
-            result_text.append(text[1])
-
-        st.write("Extracted Text:")
-        st.write(result_text)
-
-        # Join the extracted text for ChatGPT query
-        query = " ".join(result_text)
-
-        # Use ChatGPT to get a response
-        if query:
-            st.write("Bard response:")
-            model = genai.GenerativeModel('gemini-pro-vision')
-            response=model.generate_content(["what is the answer", input_image])
-            st.write(response.text)
-
+st.write("Bard response:")
+model = genai.GenerativeModel('gemini-pro-vision')
+response=model.generate_content(["what is the answer", input_image])
+st.write(response.text)
 # Release camera when done
 if input_option == "Camera":
     cap.release()
