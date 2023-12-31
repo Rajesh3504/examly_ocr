dc38ad4d2837e3015dbe79066d2a331d98dd9af7
RAJESH PONNAGANTI <139054007+Rajesh3504@users.noreply.github.com>
2023-12-31 20:05:28 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,13 +2,15 @@ import easyocr as ocr
 import streamlit as st  
 from PIL import Image  
 import numpy as np  
-
 import cv2  
 import google.generativeai as genai
 
 genai.configure(api_key='AIzaSyCbrGlCX60mFMpwmmB_jYJGZs7M21PpTj4')
 
-st.title("Chaduvko bro")
+# title
+st.title("Answers kavala bro")
+
+
 
 st.markdown("")
 
@@ -21,30 +23,45 @@ if input_option == "Camera":
 
     if st.button("Capture Frame"):
         ret, frame = cap.read()
-        image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        input_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
 elif input_option == "File":
     # File input
     image = st.file_uploader(label="Upload your image here", type=['png', 'jpg', 'jpeg'])
 
     if image is not None:
-        image = Image.open(image)
+        input_image = Image.open(image)
 
 # Load OCR model
-st.write("Answer:")
+@st.cache_resource
+def load_model():
+    reader = ocr.Reader(['en'], model_storage_directory='.')
+    return reader
+
+reader = load_model()  # Load model
 model = genai.GenerativeModel('gemini-pro-vision')
-ans="what is the answer"
-response = model.generate_content([ans, image])
+if 'input_image' in locals():
+    st.image(input_image)  # Display image
+
+    with st.spinner("Please wait, processing..."):
+
+        result = reader.readtext(np.array(input_image))
+
+        result_text = []  # Empty list for results
+
+        for text in result:
+            result_text.append(text[1])
+
+        st.write("Extracted Text:")
+        st.write(result_text)
+
+        # Join the extracted text for ChatGPT query
+        query = " ".join(result_text)
+
+        # Use ChatGPT to get a response
+        if query:
+             model = genai.GenerativeModel('gemini-pro')
+             response = model.generate_content(query)
 st.write(response.text)
-
-prompt = st.text_input("Enter your prompt:")
-
-if st.button("Generate Response"):
-        
-        response1 = model.generate_content([prompt, image])
-
-st.write(response1.text)
-
-# Release camera when done
 if input_option == "Camera":
     cap.release()
