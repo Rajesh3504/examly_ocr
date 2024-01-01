b78e78ddc1bfe32aecf55b3b08ab722cc54e5df8
RAJESH PONNAGANTI <139054007+Rajesh3504@users.noreply.github.com>
2024-01-01 11:21:55 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,19 +2,17 @@ import easyocr as ocr
 import streamlit as st  
 from PIL import Image  
 import numpy as np  
+
 import cv2  
 import google.generativeai as genai
 
 genai.configure(api_key='AIzaSyCbrGlCX60mFMpwmmB_jYJGZs7M21PpTj4')
 
-# title
-st.title("Answers kavala bro")
-
-
+st.title("Chaduvko bro")
 
 st.markdown("")
 
-# Input selection: Camera or File
+#    Input selection: Camera or File
 input_option = st.radio("Select Input Source:", ["Camera", "File"])
 
 if input_option == "Camera":
@@ -23,45 +21,31 @@ if input_option == "Camera":
 
     if st.button("Capture Frame"):
         ret, frame = cap.read()
-        input_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
 elif input_option == "File":
     # File input
+    image=''
     image = st.file_uploader(label="Upload your image here", type=['png', 'jpg', 'jpeg'])
 
     if image is not None:
-        input_image = Image.open(image)
+        image = Image.open(image)
 
-# Load OCR model
-@st.cache_resource
-def load_model():
-    reader = ocr.Reader(['en'], model_storage_directory='.')
-    return reader
+st.spinner("Ruko zara sabar karo")
+st.image(image)
 
-reader = load_model()  # Load model
+st.write("Answer:")
 model = genai.GenerativeModel('gemini-pro-vision')
-if 'input_image' in locals():
-    st.image(input_image)  # Display image
-
-    with st.spinner("Please wait, processing..."):
-
-        result = reader.readtext(np.array(input_image))
-
-        result_text = []  # Empty list for results
-
-        for text in result:
-            result_text.append(text[1])
-
-        st.write("Extracted Text:")
-        st.write(result_text)
-
-        # Join the extracted text for ChatGPT query
-        query = " ".join(result_text)
-
-        # Use ChatGPT to get a response
-        if query:
-             model = genai.GenerativeModel('gemini-pro')
-             response = model.generate_content(query)
+response = 'answer'
+answer="what is the answer"
+response = model.generate_content([answer, image])
 st.write(response.text)
+
+prompt = st.text_input("Enter your prompt:")
+response1 = 'answer'
+if st.button("Generate Response"):
+        response1= model.generate_content([prompt, image])
+        st.write(response1.text)
+# Release camera when done
 if input_option == "Camera":
     cap.release()
